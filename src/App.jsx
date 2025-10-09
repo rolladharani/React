1e5cf18bfd1f2b2167dd5abb2dcb23bcfe3f7849
rolladharani <rolladharani77@gmail.com>
2025-10-09 11:48:35 +0530

    ChANGE1

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,66 @@
     import { useState } from 'react'
     import reactLogo from './assets/react.svg'
     import viteLogo from '/vite.svg'
+    import { useEffect } from 'react';
+    import { BrowserRouter,Routes,Route } from 'react-router-dom';
     // import './App.css'
-    import './Cards.css'
-    import FirstName from './FirstName'
-    import LastName from './LastName'
-    import Email from './Email'
-    import PhoneNumber from './PhoneNumber'
-    import DOB from './DOB'
-    import Gender from './Gender'
-    import Comp1 from './Component1'
-    import Comp2 from './Component2'
+    // import './Cards.css'
+    // import FirstName from './FirstName'
+    // import LastName from './LastName'
+    // import Email from './Email'
+    // import PhoneNumber from './PhoneNumber'
+    // import DOB from './DOB'
+    // import Gender from './Gender'
+    // import Comp1 from './Component1'
+    // import Comp2 from './Component2'
+    import Page1 from './page1';
+    import Page2 from "./page2";
+    import Store from "./Store";
 
     function App() {
-      const [count, setCount] = useState(0)
-      var data=[
-        {
-          "name":"THUB",
-          varient:"green"
-        },
-        {
-          "name":"aditya",
-          varient:"red"
-        },
-        {
-          "name":"Pragati",
-          varient:"red"
-        }
-      ];
+        const [Count ,setCount] = useState(0)
+    return (
+            <>
+            <Store.Provider value={{Count,setCount}}>
+              <BrowserRouter>
+                <Routes>
+                  <Route path="/" element={<Page1 />} />
+                  <Route path="/page2" element={<Page2 />} />
+                </Routes>
+              </BrowserRouter>
+            </Store.Provider>
+            </>
+          );
+}
+export default App
+      // const [count, setCount] = useState(0)
+      // var data=[
+      //   {
+      //     "name":"THUB",
+      //     varient:"green"
+      //   },
+      //   {
+      //     "name":"aditya",
+      //     varient:"red"
+      //   },
+      //   {
+      //     "name":"Pragati",
+      //     varient:"red"
+      //   }
+      // ];
     
-        return(
-          <>
-            <div className="parent">
-        {
-        data.map((ele) => (
-          <div className={ele.varient == "green" ? "card bg-green" : "card bg-red"}>
-            {ele.name}
-          </div>
-        ))
-        }
-      </div>
+      //   return(
+      //     <>
+      //       <div className="parent">
+      //   {
+      //   data.map((ele) => (
+      //     <div className={ele.varient == "green" ? "card bg-green" : "card bg-red"}>
+      //       {ele.name}
+      //     </div>
+      //   ))
+      //   }
+      // </div>
 
-          </>
-        )
-    }
-    export default App
+      //     </>
+      //   )
+
